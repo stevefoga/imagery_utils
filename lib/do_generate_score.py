5d9ccdba7d6be1e102568c045bbabda7bd97ef3d
Steven C Foga <sfoga@umn.edu>
2018-03-14 08:13:20 -0500

    logging additions; fixed argparse logic; made sure src is read as a dir if dir is supplied

--- lib/do_generate_score.py
+++ lib/do_generate_score.py
@@ -21,8 +21,22 @@ logger.setLevel(logging.DEBUG)
 
 def main(src, pct_thresh, overwrite_invalid, overwrite_all, not_tiled, dryrun, water_mask=None, tile_path=None):
 
+    logfile = os.path.join(os.path.dirname(src), 'generate_score.log')
+    lfh = logging.FileHandler(logfile)
+    lfh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s- %(message)s', '%m-%d-%Y %H:%M:%S')
+    lfh.setFormatter(formatter)
+    logger.addHandler(lfh)
+
+    lsh = logging.StreamHandler()
+    lsh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s- %(message)s','%m-%d-%Y %H:%M:%S')
+    lsh.setFormatter(formatter)
+    logger.addHandler(lsh)
+
     # if dir, get files, else assume single file was supplied
     if os.path.isdir(src):
+        src = os.path.join(src, '')
         fns_in = glob.glob(src + "*.tif")
     else:
         fns_in = [src]
@@ -32,11 +46,14 @@ def main(src, pct_thresh, overwrite_invalid, overwrite_all, not_tiled, dryrun, w
         # if all files are not to be modified, check for invalid values
         if not overwrite_all:
             score_path = fn_in.replace('.tif', '.score')
+            logger.debug("score_path: {0}".format(score_path))
 
             if os.path.isfile(score_path) and overwrite_invalid:
                 # check if score is invalid
                 with open(score_path, 'r') as f:
                     score = float(f.read())
+
+                logger.debug("score: {0}".format(score))
                 if score != -9999:
                     logger.debug('score is valid, excluding {0}'.format(fn_in))
                     continue
@@ -69,7 +86,7 @@ if __name__ == "__main__":
                         required=False)
     parser.add_argument("--overwrite-all", help="Overwrite all .score files", action="store_true", required=False)
     parser.add_argument("--not-tiled", help="Use if mosaic is single image (as opposed to being split into chunks)",
-                        action="store_false")
+                        action="store_true")
 
     parser.add_argument("--dryrun", help="Print cmd, do not submit job", dest="dryrun", action="store_true",
                         required=False)
